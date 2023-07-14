ab7ad0de87301f0b43d4872549f0e1bac5930091
Victor Moreira <136837606+victormoreirak@users.noreply.github.com>
2023-07-14 16:24:54 -0300

    correção portfólio

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 500;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add('show-animate')
+        }
+
+        else {
+            sec.classList.remove('show-animate');
+        }
+    })
+}
+
 window.addEventListener("scroll", function(){
     var header = document.querySelector('#header')
     header.classList.toggle('scroll-down',window.scrollY > 0)
